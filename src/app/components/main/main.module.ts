e55c7301b201812eae4818deec0155c0a744bff4
JohnPaulM@m@ni <U201919105@upc.edu.pe>
2023-04-29 01:43:52 -0500

    search
    search

--- src/app/components/main/main.module.ts
+++ src/app/components/main/main.module.ts
@@ -7,13 +7,15 @@ import { AccountComponent } from './account/account.component';
 import { OffersComponent } from './offers/offers.component';
 import { SharedModule } from '../shared/shared.module';
 import { AddServiceComponent } from './add-service/add-service.component';
+import { SearchComponent } from './search/search.component';
 
 @NgModule({
   declarations: [
     DashboardComponent,
     AccountComponent,
     OffersComponent,
-    AddServiceComponent
+    AddServiceComponent,
+    SearchComponent
   ],
   imports: [
     CommonModule,
