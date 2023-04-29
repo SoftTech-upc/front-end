ac02498a65a71b265d5d110b4564d7bd882f5ba5
Jerry Quispe Gavilan <U20201c043@upc.edu.pe>
2023-04-29 01:19:09 -0500

    Update Card.ts

--- src/app/Clases/Card.ts
+++ src/app/Clases/Card.ts
@@ -1,12 +1,31 @@
-export class Card {
-    img: string;
-    description: string;
-    count_stars: number;
-    coste: number[];
-    constructor(img: string,description: string,count_stars: number,coste1: number[]){
-        this.img=img;
-        this.description=description;
-        this.count_stars=count_stars;
-        this.coste=coste1
-    }
+import { OffersComponent } from '../components/main/offers/offers.component';
+
+export class ServiceClass {
+  id: number;
+  name: string;
+  score: number; //estrellas
+  price: number;
+  new_price: number;
+  location: string;
+  creation_date: string;
+  photo: string;
+  is_ofter: boolean;
+  description: string;
+  agencia_id: number;
+  constructor(id: number,name: string,score: number,price: number,new_price: number,
+                location: string,creation_date: string,photo: string,is_ofter: boolean,
+                description: string,agencia_id: number) 
+    {
+    this.id = id;
+    this.name = name;
+    this.score = score;
+    this.price = price;
+    this.new_price = new_price;
+    this.location = location;
+    this.creation_date = creation_date;
+    this.photo = photo;
+    this.is_ofter = is_ofter;
+    this.description = description;
+    this.agencia_id = agencia_id;
+  }
 }
