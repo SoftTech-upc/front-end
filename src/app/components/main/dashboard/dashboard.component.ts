090166fab8292be60a54ec128d13f72fa6a30d08
Jerry Quispe Gavilan <U20201c043@upc.edu.pe>
2023-04-29 01:22:08 -0500

    Update dashboard.component.ts

--- src/app/components/main/dashboard/dashboard.component.ts
+++ src/app/components/main/dashboard/dashboard.component.ts
@@ -10,25 +10,11 @@ import { ServicesService } from '../../../services/services.service';
 export class DashboardComponent implements OnInit{
 
   data_: any;
-  // img: string="../../../../assets/img/target.jpg"
-  // description:string="Disfruta de una aventura en la montaña El Huascaran"
-  // count_stars:number=4;//implementar funcion
-  // coste: number[]=[1200,1300]
 
   constructor(private ser: ServicesService){}
 ngOnInit(): void {
-  // for(let i:number=0; i<3;i++){
-  //   this.data.push(new Card(this.img,this.description,this.count_stars,this.coste));
-  // }
 
-  // console.log(this.data)
-
-
-  this.ser.get()
-  .subscribe((data: any)=>{
-    this.data_= data;
-    console.log(this.data_)
-  })
+  this.getDates()
 }
 
 getDates(){
