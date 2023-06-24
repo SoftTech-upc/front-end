8a1dfdffd4bbee65b2f689306194853f9e5efe46
JackelineSalome <89036070+JackelineSalome@users.noreply.github.com>
2023-06-24 15:46:17 -0500

    add another change on tourist profile

--- src/app/tourists/pages/tourist-profile/tourist-profile.component.ts
+++ src/app/tourists/pages/tourist-profile/tourist-profile.component.ts
@@ -7,6 +7,9 @@ import { ReservationService } from 'src/app/reservations/services/reservation.se
 import { Reservation } from 'src/app/reservations/model/reservation';
 import { TourReview } from 'src/app/reviews/model/tour-review';
 import { TourReviewService } from 'src/app/reviews/services/tour-review.service';
+import { AgencyReviewService } from 'src/app/reviews/services/agency-review.service';
+import { AgencyReview } from 'src/app/reviews/model/agency-review';
+import { Agency } from 'src/app/agencies/model/agency';
 
 @Component({
   selector: 'app-tourist-profile',
@@ -20,17 +23,19 @@ export class TouristProfileComponent implements OnInit {
   reservations1: Reservation[];
   reservationId: number;
   tourReview1: TourReview[]; 
+  agencyReview1: AgencyReview[]; 
 
   @ViewChild('touristForm', {static: false})
   touristForm!: NgForm;
 
-  constructor(private touristService: TouristService, private tourReviewService: TourReviewService, private reservationService: ReservationService, private route: ActivatedRoute) {
+  constructor(private touristService: TouristService, private agencyReviewService: AgencyReviewService, private tourReviewService: TourReviewService, private reservationService: ReservationService, private route: ActivatedRoute) {
     this.touristData = {} as Tourist
     this.edit = false
     this.tourist_id = this.route.snapshot.paramMap.get('id');
     this.reservations1 = [];
     this.reservationId = 0;
     this.tourReview1 = [];
+    this.agencyReview1 = [];
   }
 
   ngOnInit(): void {
@@ -38,6 +43,7 @@ export class TouristProfileComponent implements OnInit {
       this.touristData = response
       this.getReservationByTouristId(this.touristData.id);
       this.getTourReviewByTouristId(this.touristData.id);
+      this.getAgencyReviewByTouristId(this.touristData.id);
     })
   }
 
@@ -73,6 +79,18 @@ export class TouristProfileComponent implements OnInit {
         }
       });
       this.tourReview1 = tourRevieww;
+    });
+  }
+
+  getAgencyReviewByTouristId(tourId: number): void {
+    this.agencyReviewService.getAgencyReviewByTourId(tourId).subscribe((response: any) => {
+      const agencyRevieww: AgencyReview[] = [];
+      response.forEach((agencyReview: AgencyReview) => {
+        if (agencyReview.customer_id === tourId) {
+          agencyRevieww.push(agencyReview);
+        }
+      });
+      this.agencyReview1 = agencyRevieww;
     });
   }
 }
